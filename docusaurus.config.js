b38195ea8d1da8ed11c9c7cb6f2eb8dbdc49dbc9
PC <PC>
2020-04-01 14:20:44 +0800

    feat: 重构phoenix的首页

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,81 +1,95 @@
 module.exports = {
-    title: 'Phoenix',
-    tagline: 'phoenix是一个消息驱动的有状态微服务框架',
-    url: 'https://github.com/PhoenixIQ/PhoenixIQ.github.io',
-    baseUrl: '/',
-    favicon: 'img/phoenix.png',
-    organizationName: 'PhoenixIQ', // Usually your GitHub org/user name.
-    projectName: 'PhoenixIQ.github.io', // Usually your repo name.
-    themeConfig: {
-        sidebarCollapsible: false,
-        navbar: {
-            title: 'Phoenix',
-            logo: {
-                alt: 'Phoenix Logo',
-                src: 'img/phoenix.png',
-            },
-            links: [
-                {to: 'docs/phoenix-2.x/phoenix/phoenix-2x', label: '文档', position: 'left'},
-                {to: 'blog/rocketmq-in-phoenix', label: '博客', position: 'left'},
-                {to: 'docs/phoenix-2.x/phoenix/phoenix-download-2x', label: '版本', position: 'left'},
-                {to: 'https://github.com/PhoenixIQ', label: 'GitHub', position: 'right'},
-            ],
-        },
-        footer: {
-            style: 'dark',
-            links: [
-                {
-                    title: '文档',
-                    items: [
-                        {
-                            label: 'Phoenix是什么?',
-                            to: 'docs/phoenix-2.x/phoenix/phoenix-2x',
-                        },
-                        {
-                            label: '快速入门',
-                            to: 'docs/phoenix-2.x/quick-start/phoenix-lite-2x',
-                        },
-                        {
-                            label: 'FAQ',
-                            to: 'docs/phoenix-2.x/phoenix/phoenix-faq-2x',
-                        },
-                    ],
-                },
-                {
-                    title: 'Example',
-                    items: [
-                        {
-                            label: '账户管理',
-                            href: 'https://github.com/PhoenixIQ/bank-account',
-                        },
-                    ],
-                },
-                {
-                    title: '资源',
-                    items: [
-                        {
-                            label: '博客',
-                            to: 'blog',
-                        },
-                    ],
-                },
-            ],
-            copyright: `Copyright Â© ${new Date().getFullYear()} Phoenix Group`,
-        },
+  title: 'Phoenix',
+  tagline: 'Phoenix',
+  url: 'http://phoenix-website.sz.iquantex.com/',
+  baseUrl: '/',
+  favicon: 'img/phoenix.png',
+  organizationName: 'quantex', // Usually your GitHub org/user name.
+  projectName: 'phoenix', // Usually your repo name.
+  themeConfig: {
+    disableDarkMode: true,
+    sidebarCollapsible: false,
+    onPageNav: 'separate',
+    navbar: {
+      title: '',
+      logo: {
+        alt: 'Phoenix Logo',
+        src: 'img/logo.png',
+      },
+      links: [
+        { to: 'http://localhost:3000/', label: '首页', position: 'right' },
+        { to: 'docs/phoenix-2.x/phoenix/phoenix-2x', label: '文档', position: 'right' },
+        { to: 'blog/rocketmq-in-phoenix', label: '博客', position: 'right' },
+        { to: 'docs/phoenix-2.x/phoenix/phoenix-download-2x', label: '版本', position: 'right' },
+        { to: '', label: '联系我们', position: 'right' },
+      ],
     },
-    presets: [
-        [
-            '@docusaurus/preset-classic',
+    footer: {
+      style: 'dark',
+      logo: {
+        src: 'img/footer.png',
+      },
+      links: [
+        {
+          title: '文档',
+          items: [
             {
-                docs: {
-                    sidebarPath: require.resolve('./sidebars.js'),
-                    editUrl:
-                        'http://phoenix-website.sz.iquantex.com/',
-                },
-                theme: {
-                    customCss: require.resolve('./src/css/custom.css'),
-                },
+              label: '白皮书',
+              to: 'docs/phoenix-2.x/phoenix/phoenix-2x',
             },
-        ],
+            {
+              label: '快速入门',
+              to: 'docs/phoenix-2.x/quick-start/phoenix-lite-2x',
+            },
+            {
+              label: 'FAQ',
+              to: 'docs/phoenix-2.x/phoenix/phoenix-faq-2x',
+            },
+          ],
+        },
+        {
+          title: '案例',
+          items: [
+            {
+              label: '银行转账',
+              href: 'https://github.com/PhoenixIQ/bank-account',
+            },
+            {
+              label: '交易风控',
+              href: 'https://github.com/PhoenixIQ/bank-account',
+            },
+            {
+              label: '炒股大赛',
+              href: 'https://github.com/PhoenixIQ/bank-account',
+            },
+          ],
+        },
+        {
+          title: '资源',
+          items: [
+            {
+              label: '博客',
+              to: 'blog',
+            },
+          ],
+        },
+      ],
+      copyright: `北京宽拓智融科技有限公司 Copyright @2013 - ${new Date().getFullYear()} iQuantex.com All Rights Reserved. 宽拓公司 版权所有`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'http://phoenix-website.sz.iquantex.com/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
     ],
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,100 +2,46 @@ import React from 'react';
 import classnames from 'classnames';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
 
-const features = [
+const features_1 = [
   {
-    title: <>内存计算</>,
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
-    description: (
-      <>
-         Phoenix使系统业务数据的修改完全在内存中进行，业务数据模型和算法面向内存构建，使业务代码高度聚合，并且简化计算层和存储层的交互提升性能。
-      </>
-    ),
+    title: '高性能',
+    imageUrl: 'img/gxn.png',
+    description: 'Phoenix的强大内存计算引擎可以提供毫秒级的低时延，其分布式并行能力保证处理复杂场景时仍然可以提供高达50K的TPS',
   },
   {
-    title: <>消息驱动</>,
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
-    description: (
-      <>
-         Phoenix使用消息进行服务间通信，各服务自定义消息和发布消息。消息驱动是异步的交互方式，基于Fire-and-Forget原则，不必阻塞式地等待相关的处理结果。
-      </>
-    ),
+    title: '高可用',
+    imageUrl: 'img/gky.png',
+    description: 'Phoenix提倡“let it crash”的理念，依托事件溯源等技术做到了有状态服务下的多节点部署，满足了金融核心系统RPO=0和秒级RTO的高可用要求',
   },
   {
-    title: <>微服务</>,
-    imageUrl: 'img/undraw_docusaurus_react.svg',
-    description: (
-      <>
-        Phoenix框架同时也是可伸缩，可拆分聚合的微服务框架。根据DDD思想划分领域构建服务，对服务进行独立的开发、测试和部署。
-      </>
-    ),
+    title: '高弹性',
+    imageUrl: 'img/gtx.png',
+    description: 'Phoenix响应式架构的属性使得基于它开发的服务具有良好的水平扩展和收缩的能力，以及流量自动负载均衡的能力，表现出良好的弹性',
   },
 ];
 
-const steps = [
+const features_2 = [
   {
-    title: <>金融领域复杂业务场景解决之道</>,
-    imageUrl: 'img/what-problems-phoenix-solves.png',
-    description: (
-      <>
-        <div style={{whiteSpace: 'pre-wrap'}}>降低成本</div>
-        <ul>
-          <li>降低了系统复杂度</li>
-          <li>降低了开发人员门槛</li>
-          <li>节省了系统建设时间</li>
-        </ul>
-        增强效率
-        <ul>
-          <li>提升了系统的运行性能</li>
-          <li>实现了系统的实时监控</li>
-          <li>让系统面向开放，拥抱集成</li>
-        </ul>
-      </>
-    ),
-    position: 'right'
+    title: '高容错',
+    imageUrl: 'img/grc.png',
+    description: 'Phoenix服务内的最小运行单位是聚合根，聚合根之间是完全隔离的，故障不会互相影响。动态漂移特性使得节点发生故障时具有快速恢复能力',
+  },
+  {
+    title: '易编程',
+    imageUrl: 'img/ybc.png',
+    description: 'Phoenix提供了开箱即用的SpringBoot Starter，对事件驱动和分布式并行计算两种复杂编程模式进行了封装，大大降低了开发者的使用门槛',
+  },
+  {
+    title: '易运维',
+    imageUrl: 'img/yyw.png',
+    description: 'Phoenix提供了监控管理平台和开放接口，支持系统实时的性能监控、业务监控、调用链追踪、异常分析等能力，满足运维和金融审计需求',
   },
 ];
 
-function Step({imageUrl, title, description, position}) {
-  const imgUrl = useBaseUrl(imageUrl);
-  return (
-      <>
-        {position === 'left' && (
-          <div className="row">
-            <div className="col col--7">
-              <div className="text--center">
-                <img className={styles.stepsImage} src={imgUrl} alt={title} />
-              </div>
-            </div>
-            <div className="col col--4 col--offset-1 padding-vert--xl">
-              <h2>{title}</h2>
-              <p>{description}</p>
-            </div>
-          </div>
-        )}
-        {position === 'right' && (
-          <div className="row">
-            <div className="col col--4 padding-vert--xl">
-              <h2>{title}</h2>
-              <p>{description}</p>
-            </div>
-            <div className="col col--7 col--offset-1">
-              <div className="text--center">
-                <img className={styles.stepsImage} src={imgUrl} alt={title} />
-              </div>
-            </div>
-          </div>
-        )}
-      </>
-  );
-}
- 
-
-function Feature({imageUrl, title, description}) {
+function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
     <div className={classnames('col col--4', styles.feature)}>
@@ -104,60 +50,94 @@ function Feature({imageUrl, title, description}) {
           <img className={styles.featureImage} src={imgUrl} alt={title} />
         </div>
       )}
-      <h3>{title}</h3>
+      <h3 className="text--center">{title}</h3>
       <p>{description}</p>
     </div>
   );
 }
 
 function Home() {
-  const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <header className={classnames('hero hero--primary', styles.heroBanner)}>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className={classnames(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
-              )}
-              to={useBaseUrl('docs/phoenix-2.x/quick-start/phoenix-lite-2x')}>
-              Get Started
-            </Link>
+    <Layout>
+      <header className={classnames(styles.heroBanner)}>
+        <div className={classnames(styles.containerBanner)}>
+          <div className={classnames(styles.container)}>
+            <h1>
+              PHOENIX
+            </h1>
+            <p className={classnames(styles.content)}>
+              Phoenix是一个高性能的响应式微服务框架，致力于打造有状态的微服务框架
+            </p>
+            <div className={classnames(styles.link)}>
+              <Link className={classnames(styles.getStarted)}
+                to={useBaseUrl('docs/phoenix-2.x/quick-start/phoenix-lite-2x')}>
+                开始使用
+              </Link>
+              <Link className={classnames(styles.getStarted)}
+                to="https://github.com/PhoenixIQ/bank-account">
+                用户登记
+              </Link>
+              <Link className={classnames(styles.getStarted)}
+                to="https://github.com/PhoenixIQ">
+                GitHub
+              </Link>
+            </div>
+            <div>
+              <iframe className={classnames(styles.indexCtasGitHubButton)} src="https://ghbtns.com/github-btn.html?user=phoenixiq&repo=phoenix-website&type=star&count=true&size=large">
+                <p>Your browser does not support iframes.</p>
+              </iframe>
+              <iframe className={classnames(styles.indexCtasGitHubButton)} src="https://ghbtns.com/github-btn.html?user=phoenixiq&repo=phoenix-website&type=fork&count=true&size=large">
+                <p>Your browser does not support iframes.</p>
+              </iframe>
+            </div>
+            <div className={classnames(styles.release)}>
+              <p className={classnames(styles.version)}>Release Note of 1.1.0</p>
+              <p className={classnames(styles.date)}>
+                Released on Feb 19, 2020
+              </p>
+            </div>
+          </div>
+        </div >
+      </header >
+      <main>
+        <div className={classnames(styles.introductWrap)}>
+          <div className={classnames(styles.introduct)}>
+            <div className={classnames(styles.article)}>
+              <h2>Phoenix是什么？</h2>
+              <p>
+                Phoenix是一个有状态的响应式微服务框架。它提供了高可用、动态伸缩、分布式事务、内存管理、实时监控等关键能力。擅长解决金融领域中业务复杂，性能要求高的场景。
+            </p>
+            </div>
+            <div className={classnames(styles.conception)}>
+              <img src="img/attr.png" alt="Phoenix架构图" />
+            </div>
           </div>
         </div>
-      </header>
-      <main>
-        {features && features.length && (
+        <div className={styles.featuresWrap}>
           <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
+            <h1 className="text--center">Phoenix特性</h1>
+            {features_1 && features_1.length && (
+              <div className={classnames(styles.featuresCon)}>
+                <div className="row">
+                  {features_1.map((props, idx) => (
+                    <Feature key={idx} {...props} />
+                  ))}
+                </div>
               </div>
-            </div>
-          </section>
-        )}
-        {steps && steps.length && (
-          <section className={styles.steps}>
-            <div className="container">
-              <div className="row">
-                 {steps.map((props, idx) => (
-                     <Step key={idx} {...props} />
-                 ))}
+            )}
+            {features_2 && features_2.length && (
+              <div className={classnames(styles.featuresCon)}>
+                <div className="row">
+                  {features_2.map((props, idx) => (
+                    <Feature key={idx} {...props} />
+                  ))}
+                </div>
               </div>
-            </div>
+            )}
           </section>
-        )}
+        </div>
       </main>
-    </Layout>
+    </Layout >
   );
 }
 
